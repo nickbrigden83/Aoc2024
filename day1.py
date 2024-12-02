b15e65abc72cde4b146c4781d3921e0d5bc4f0cc
nick.b <nickbrigden83@gmail.com>
2024-12-02 16:25:08 +0000

    Day 1 Part 2

--- day1.py
+++ day1.py
@@ -5,8 +5,14 @@ class DayOne:
         sorted_lists = [sorted(lists[0]), sorted(lists[1])]
         return self.get_total_of_differences(sorted_lists)
 
+    def part2_handler(self, input_name: str):
+        lists = self.load_input_data(input_name)
+        sorted_lists = [sorted(lists[0]), sorted(lists[1])]
+        number_counts = self.generate_number_count_dict(sorted_lists[1])
+        return self.get_total_of_similarity(sorted_lists[0], number_counts)
 
-    def load_input_data(self, file_name:str) -> list[list[int]]:
+    @staticmethod
+    def load_input_data(file_name:str) -> list[list[int]]:
         list_one = []
         list_two = []
         with open(file_name, "r")  as f:
@@ -24,3 +30,21 @@ class DayOne:
             total_difference += abs(item1 - item2)
         return total_difference
 
+    @staticmethod
+    def generate_number_count_dict(list_of_numbers) -> dict[int, int]:
+        number_count_dict = {}
+        for number in list_of_numbers:
+            if number in number_count_dict.keys():
+                number_count_dict[number] += 1
+            else:
+                number_count_dict[number] = 1
+        return number_count_dict
+
+    def get_total_of_similarity(self, list_of_numbers: list[int], number_counts: dict[int, int]) -> int:
+        total_similarity = 0
+        for number in list_of_numbers:
+            try:
+                total_similarity += number * number_counts[number]
+            except KeyError:
+                continue
+        return total_similarity
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -8,3 +8,10 @@ class TestDayOne(TestCase):
         expected_result = 11
         result = test_dayone.handler("test_input_part_1.txt")
         self.assertEqual(expected_result, result)
+
+
+    def test_day_1_part_2(self):
+        test_dayone = DayOne()
+        expected_result = 31
+        result = test_dayone.part2_handler("test_input_part_1.txt")
+        self.assertEqual(expected_result, result)
